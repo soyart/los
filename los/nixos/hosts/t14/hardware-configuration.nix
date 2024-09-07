fa713790e5863e7146fe7503f57d1ff11804500e
soyart <artdrawin@gmail.com>
2024-09-07 16:29:38 +0000

    persist more files

--- los/nixos/hosts/t14/hardware-configuration.nix
+++ los/nixos/hosts/t14/hardware-configuration.nix
@@ -23,8 +23,13 @@
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
       "/var/lib/iwd"
-      # "/etc/iwd"
+      "/etc/iwd"
       # { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+    ];
+    files = [
+      "/etc/machine-id"
+      "/etc/passwd"
+      "/etc/shadow"
     ];
   };
 
