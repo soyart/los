3266f30638f9527b7863d7d5bf18d7c380fb5213
soyart <artdrawin@gmail.com>
2024-09-09 15:55:28 +0000

    more bash config defaults

--- defaults/bash.nix
+++ defaults/bash.nix
@@ -6,8 +6,20 @@ username:
   home-manager.users."${username}" = {
     programs.bash = {
       enable = true;
-      historyControl = [ "ignoredups" ];
-      historyFileSize = 512;
+      enableCompletion = true;
+
+      historyControl = [ "ignoreboth" ];
+      historyFile = null;
+      historySize = 256;
+
+      shellAliases = {
+        "c" = "clear";
+        "g" = "git";
+        "ga" = "git add";
+        "gc" = "git commit";
+        "gs" = "git status";
+        "h" = "hx";
+      };
     };
 
     home.stateVersion = "24.05";
