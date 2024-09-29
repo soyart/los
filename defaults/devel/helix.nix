f3118af187d61f8ea04cd9e1c6ef60e798ecf589
soyart <artdrawin@gmail.com>
2024-09-29 17:29:20 +0000

    add clang formatter

--- defaults/devel/helix.nix
+++ defaults/devel/helix.nix
@@ -1,11 +1,22 @@
 username:
 
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     (import ../../home/progs/helix username)
   ];
 
-  los.home."${username}".progs.helix.enable = true;
+  los.home."${username}".progs.helix = {
+    enable = true;
+    langServers = with pkgs; [
+      nixd
+      nixpkgs-fmt
+
+      go
+      gopls
+      gotools
+      go-tools
+    ];
+  };
 }
