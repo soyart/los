6ef5a5686cf1b7096552adc6dd954b2a8c082986
soyart <artdrawin@gmail.com>
2024-09-30 17:40:21 +0000

    host(t14): add lsp and formatter for shell scripts

--- hosts/t14/devel.nix
+++ hosts/t14/devel.nix
@@ -5,7 +5,11 @@ username:
 {
   los.home."${username}".progs.helix = {
     langServers = with pkgs; [
+      # By default, bash-lsp should use shellcheck and shfmt
+      bash-language-server
       shellcheck
+      shfmt
+
       clang-tools
     ];
   };
