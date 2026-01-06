ae320602c8a3972ec17aabccc3dbd64012e8a141
Prem Phansuriyanon <prem.p@lmwn.com>
2026-01-07 01:40:15 +0700

    fix: add null check for homev2/fonts

--- modules/homev2/fonts/config.nix
+++ modules/homev2/fonts/config.nix
@@ -1,13 +1,13 @@
 # Fonts config module
 
-{ lib, config, pkgs, ... }:
+{ lib, config, ... }:
 
 {
   config.home-manager.users = lib.mapAttrs (username: userCfg:
     lib.mkIf userCfg.fonts.enable {
-      home.packages = userCfg.fonts.packages;
-      fonts = lib.mkIf (userCfg.fonts.defaults != null) {
-        fontconfig = {
+      home.packages = lib.mkIf (userCfg.fonts.packages != []) userCfg.fonts.packages;
+      fonts = {
+        fontconfig = lib.mkIf (userCfg.fonts.defaults != null) {
           enable = true;
           defaultFonts = userCfg.fonts.defaults;
         };
