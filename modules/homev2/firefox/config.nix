b1267863d93bfb6d7f5cdb3b62d80a81be9e89fb
soyart <artdrawin@gmail.com>
2026-03-12 02:05:31 +0700

    firefox: more privacy settings

--- modules/homev2/firefox/config.nix
+++ modules/homev2/firefox/config.nix
@@ -30,8 +30,7 @@ in
               MOZ_ENABLE_WAYLAND = "1";
             };
 
-            # Stateless profile
-            home.file.".mozilla/firefox/profiles.ini".force = true;
+            # home.file.".mozilla/firefox/profiles.ini".force = true;
 
             programs.firefox = {
               enable = true;
@@ -40,16 +39,43 @@ in
                 then pkgs.wrapFirefox (pkgs.firefox-unwrapped.override { pipewireSupport = true; }) { }
                 else pkgs.firefox;
 
+              policies = {
+                AppAutoUpdate = false;
+                BackgroundAppUpdate = false;
+
+                DisableTelemetry = true;
+                DisableFirefoxStudies = true;
+                EnableTrackingProtection = {
+                  Value = true;
+                  Locked = true;
+                  Cryptomining = true;
+                  Fingerprinting = true;
+                };
+
+                DontCheckDefaultBrowser = true;
+                DisablePocket = true;
+                SearchBar = "unified";
+              };
+
               profiles.default = {
                 id = 0;
                 isDefault = true;
                 settings = {
+                  # Disable AI features
+                  "browser.ml.chat.enabled" = false;
+                  "browser.ml.chat.page" = false;
+                  "browser.ml.linkPreview.enabled" = false;
+                  "browser.tabs.groups.smart.enabled" = false;
+                  "browser.tabs.groups.smart.userEnabled" = false;
+                  "browser.translations.enable" = false;
                   "browser.ai.control.default" = "blocked";
                   "browser.ai.control.linkPreviewKeyPoints" = "blocked";
                   "browser.ai.control.pdfjsAltText" = "blocked";
                   "browser.ai.control.sidebarChatbot" = "blocked";
                   "browser.ai.control.smartTabGroups" = "blocked";
                   "browser.ai.control.translations" = "blocked";
+                  "extensions.ml.enabled" = false;
+                  "pdfjs.enableAltText" = false;
                 };
               };
             };
