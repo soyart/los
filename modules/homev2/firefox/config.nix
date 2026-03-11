66eb4532ac7b2887cc13f7a32aaebec912704bbf
soyart <artdrawin@gmail.com>
2026-03-12 02:05:31 +0700

    firefox: disable more

--- modules/homev2/firefox/config.nix
+++ modules/homev2/firefox/config.nix
@@ -44,12 +44,20 @@ in
                 id = 0;
                 isDefault = true;
                 settings = {
+                  "browser.ml.chat.enabled" = false;
+                  "browser.ml.chat.page" = false;
+                  "browser.ml.linkPreview.enabled" = false;
+                  "browser.tabs.groups.smart.enabled" = false;
+                  "browser.tabs.groups.smart.userEnabled" = false;
+                  "browser.translations.enable" = false;
                   "browser.ai.control.default" = "blocked";
                   "browser.ai.control.linkPreviewKeyPoints" = "blocked";
                   "browser.ai.control.pdfjsAltText" = "blocked";
                   "browser.ai.control.sidebarChatbot" = "blocked";
                   "browser.ai.control.smartTabGroups" = "blocked";
                   "browser.ai.control.translations" = "blocked";
+                  "extensions.ml.enabled" = false;
+                  "pdfjs.enableAltText" = false;
                 };
               };
             };
