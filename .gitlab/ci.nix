cc3313a38320a211bab1eea381e1dcc2958846f9
soyart <artdrawin@gmail.com>
2026-08-25 00:22:08 +0700

    fix: string

--- .gitlab/ci.nix
+++ .gitlab/ci.nix
@@ -68,19 +68,17 @@
                 GIT_DEPTH = "0";
               };
               script = [
-                ''
-                  set -euo pipefail
-
-                  TAG="gitlab-ci-$(date +%Y-%m-%d)-${CI_COMMIT_SHORT_SHA:?}"
-                  git remote set-url origin "https://gitlab-ci-token:${LOS_GITLAB_WRITE_TOKEN:?}@${CI_SERVER_HOST:?}/${CI_PROJECT_PATH:?}.git"
-                  if git rev-parse "refs/tags/$TAG" >/dev/null 2>&1; then
-                    echo "Tag $TAG already exists; skipping."
-                    exit 0
-                  fi
-
-                  git tag "$TAG" "$CI_COMMIT_SHA"
-                  git push origin "refs/tags/$TAG"
-                ''
+              ''
+                set -euo pipefail
+                TAG="gitlab-ci-$(date +%Y-%m-%d)-''${CI_COMMIT_SHORT_SHA:?}"
+                git remote set-url origin "https://gitlab-ci-token:''${LOS_GITLAB_WRITE_TOKEN:?}@''${CI_SERVER_HOST:?}/''${CI_PROJECT_PATH:?}.git"
+                if git rev-parse "refs/tags/$TAG" >/dev/null 2>&1; then
+                  echo "Tag $TAG already exists; skipping."
+                  exit 0
+                fi
+                git tag "$TAG" "$CI_COMMIT_SHA"
+                git push origin "refs/tags/$TAG"
+              ''
               ];
             };
           };
